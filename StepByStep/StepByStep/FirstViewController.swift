87b995096ab4fcebafef782125c32301c76cbf13
frank bassard <f.bassard@gmail.com>
2014-09-13 12:49:12 +0200

    Pics & Rm

--- StepByStep/StepByStep/FirstViewController.swift
+++ StepByStep/StepByStep/FirstViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FirstViewController.swift
 //  StepByStep
 //
 //  Created by Frank Bassard on 13/09/2014.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
